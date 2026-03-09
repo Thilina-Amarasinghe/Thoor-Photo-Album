668c2398f8e6de617c23f01f6a3c9cb86a03a69f
Thilina-Amarasinghe <thilina.dlb@gmail.com>
2026-03-09 13:27:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-$(document).ready(function(){
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A4 Flip Book with Sound</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- jQuery and turn.js from CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/turn.js/4.1.0/turn.min.js"></script>
+</head>
+<body>
+    <div class="book" id="book">
+        <div class="page"><img src="images/page1.jpg" alt="page 1"></div>
+        <div class="page"><img src="images/page2.jpg" alt="page 2"></div>
+        <div class="page"><img src="images/page3.jpg" alt="page 3"></div>
+        <div class="page"><img src="images/page4.jpg" alt="page 4"></div>
+        <!-- add more pages as needed (even number) -->
+    </div>
 
-$("#book").turn({
-
-width:900,
-
-height:600,
-
-autoCenter:true,
-
-gradients:true,
-
-acceleration:true,
-
-display:'double',
-
-when:{
-turning:function(event,page,view){
-console.log("Page turning",page);
-}
-}
-
-});
-
-});
+    <script src="script.js"></script>
+</body>
+</html>
